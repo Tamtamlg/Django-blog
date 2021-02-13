970448ffc2d15d9310c42bb6a0f7dc575b39b801
Vitaliy Podrez <tamtamlg@gmail.com>
2021-02-13 22:48:34 +0200

    category page

--- backend/news/views.py
+++ backend/news/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'News list'
+        'title': 'News list',
+        'categories': categories
     }
     return render(request, 'news/index.html', context)
 
-# def test(request):
-#     return HttpResponse('hello test')
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'category': category,
+        'categories': categories
+    }
+    return render(request, 'news/category.html', context)
