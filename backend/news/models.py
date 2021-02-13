97f4bba6fb5ea166b900f2a776f77dcc1aef93a9
Vitaliy Podrez <tamtamlg@gmail.com>
2021-02-13 11:29:21 +0200

    add bootstrap

--- backend/news/models.py
+++ backend/news/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class News(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
@@ -9,6 +10,8 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey(
+        'Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -17,3 +20,15 @@ class News(models.Model):
         verbose_name = 'News'
         verbose_name_plural = 'News'
         ordering = ['-created_at', 'title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
